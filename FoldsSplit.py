29aa82d9f124355a9e9cc844d8f4e5799360fb0d
DavidTerroso <davidterroso@live.com.pt>
2024-12-26 16:15:20 +0000

    k fold split for retouch dataset

---
+++ FoldsSplit.py
@@ -0,0 +1,88 @@
+from os import walk
+from random import shuffle
+from sklearn.model_selection import KFold
+import numpy as np
+from csv import writer
+
+def k_fold_split(k=5, folders_path=""):
+    """
+    k_fold_split iterates through the directories of the RETOUCH dataset and 
+    extracts the volumes that belong to each vendor and splits in k folds
+    according to k value specified in the arguments
+
+    Args:
+        k (int): number of k folds
+        folders_path (string): absolute or relative path to the RETOUCH dataset 
+        location
+
+    Return: 
+        None
+    """
+    # Extracts the volumes available for training and appends
+    # it to a dictionary
+    dictionary = {}
+    for (root, _, _) in walk(folders_path):
+        type = root.split("-")
+        if ((len(type) == 3) and (type[1] == "TrainingSet")):
+            vendor_volume = type[2].split("""\\""")
+            if len(vendor_volume) == 1:
+                dictionary[vendor_volume[0]] = []
+            elif len(vendor_volume) == 2:
+                vendor = vendor_volume[0]
+                volume = vendor_volume[1][-3:]
+                dictionary[vendor].append(volume)
+    
+    # Initiates KFold object from sklearn that splits the
+    # dataset with provided k value
+    kf = KFold(n_splits=k)
+    all_train_folds = []
+    all_test_folds = []
+    for key in dictionary:
+        # Shuffles the list of volumes available
+        tmp_list = dictionary[key]
+        shuffle(tmp_list)
+        dictionary[key] = tmp_list
+        # Splits in train and test according to the number of folds
+        vendor_train_folds = []
+        vendor_test_folds = []
+        for i, (train_index, test_index) in enumerate(kf.split(dictionary[key])):
+            # print("Fold:", i + 1)
+            train_volumes = np.array(dictionary[key])[train_index]
+            # print("{} Train: {}".format(key, train_volumes))
+            test_volumes = np.array(dictionary[key])[test_index]
+            # print("{} Test: {}".format(key, test_volumes))
+            vendor_train_folds.append(train_volumes)
+            vendor_test_folds.append(test_volumes)
+        all_train_folds.append(vendor_train_folds)
+        all_test_folds.append(vendor_test_folds)
+
+    # Joins all the volumes from the same fold of different vendors in one list
+    train_folds = []
+    test_folds = []
+    for j in range(k):
+        tmp_list_train = []
+        tmp_list_test = []
+        for l in range(len(dictionary.keys())):
+            tmp_list_train = tmp_list_train + all_train_folds[l][j].tolist()
+            tmp_list_test = tmp_list_test + all_test_folds[l][j].tolist()
+        train_folds.append(tmp_list_train)
+        test_folds.append(tmp_list_test)
+    shuffle(train_folds)
+    shuffle(test_folds)
+
+    # Saves the results from the split in a CSV file just for the train
+    with open("./outputs/train_splits.csv", "w", newline="") as f:
+        write = writer(f)
+        write.writerows(train_folds)    
+    
+    # Saves the results from the split in a CSV file just for the test
+    with open("./outputs/test_splits.csv", "w", newline="") as f:
+        write = writer(f)
+        write.writerows(test_folds)
+
+if __name__ == "__main__":
+    # Declares the path and the k-value, and calls the function
+    RETOUCH_path = "D:\D\RETOUCH"
+    k = 5
+    k_fold_split(k=k, folders_path=RETOUCH_path)
+    print("EOF.")
