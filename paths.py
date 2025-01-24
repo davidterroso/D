e80d31926d3f5dc0b080ce7bd531bca783da5c1c
DavidTerroso <davidterroso@live.com.pt>
2025-01-24 11:41:46 +0000

    Re-add paths.py to tracking

--- paths.py
+++
@@ -1,9 +0,0 @@
-# Paths declaration used in this project
-# Since this project may be ran on multiple
-# devices, this file will not be updated on git
-
-# Path to the RETOUCH dataset
-RETOUCH_PATH = "D:\RETOUCH"
-# Path to the folder where 
-# images are going to be stored
-IMAGES_PATH = "D:\D"
