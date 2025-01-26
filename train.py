435c05d827fad05819e3790ef781c9193868ae0c
DavidTerroso <davidterroso@live.com.pt>
2025-01-26 15:53:33 +0000

    checks model arg

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from torch.utils.data import DataLoader
+from networks.unet import UNet
 
 def train_model (
         model,
@@ -8,7 +9,9 @@ def train_model (
         learning_rate,
         optimizer,
         number_of_classes,
-        number_of_channels
+        number_of_channels,
+        fold_test,
+        tuning
 ):
     """
     Function that trains the deep learning models.
@@ -28,7 +31,40 @@ def train_model (
         model is supposed to output
         number_of_channels (int): number of channels the 
         input will present
+        fold_test (int): number of the fold that will be used 
+        in the network testing 
+        tuning (bool): indicates whether this train will
+        be performed to tune the hyperparameters or not
     
     Return:
         None
     """
+
+    # Dictionary of models, associates a string to a 
+    # PyTorch module
+    models = {
+        "UNet": "UNet()"
+        # "UNet3":
+        # "2.5D":
+    }
+
+    # Checks whether the selected model exists or not
+    if model not in models.keys():
+        print("Model not recognized. Possible models:")
+        for key in models.keys():
+            print(key)
+        return 0
+    
+if __name__ == "__main__":
+    train_model(
+        model="UNet",
+        device="GPU",
+        epochs=100,
+        batch_size=32,
+        learning_rate=2e-5,
+        optimizer="Adam",
+        number_of_classes=4,
+        number_of_channels=1,
+        fold_test=1,
+        tuning=False
+    )
