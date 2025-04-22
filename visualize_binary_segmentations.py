04447fb172185bfe47894727a003547e644007f6
DavidTerroso <davidterroso@live.com.pt>
2025-04-22 15:56:52 +0100

    code improvements and comments #107

--- visualize_binary_segmentations.py
+++ visualize_binary_segmentations.py
@@ -3,6 +3,7 @@ from IPython import get_ipython
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 from numpy import array, float32, nan, uint8, where, zeros_like
+from os import makedirs
 from pandas import read_csv
 from torch.utils.data import DataLoader
 from typing import List, Union
@@ -81,52 +82,86 @@ def visualize_binary_segmentations(volume_index: int, binary_split: bool,
         86: 0,
     }
 
+    # Dictionary that associates 
+    # a label with a variable
     mask_dict = {
         1: irf_preds,
         2: srf_preds,
         3: ped_preds
     }
 
-
+    # In case the split used specifically for the binary fluid segmentation 
+    # is used three splits have to be checked
     if binary_split:
-        folder_to_save = IMAGES_PATH + "\\binary_segmentation_binary_split\\"
+        # Declares the folder in which the images will be saved
+        folder_to_save = IMAGES_PATH + f"\\OCT_images\\prediction\\binary_segmentation_binary_split\\"
         
+        # Reads the splits for the splits used in the binary segmentation
         irf_split = read_csv("splits\\competitive_fold_selection_IRF.csv")
         srf_split = read_csv("splits\\competitive_fold_selection_SRF.csv")
         ped_split = read_csv("splits\\competitive_fold_selection_PED.csv")
 
+        # Gets the name of the fold in which the volume selected to segment 
+        # is located
         irf_fold = irf_split.columns[irf_split.isin([volume_index]).any()][0]
         srf_fold = srf_split.columns[srf_split.isin([volume_index]).any()][0]
         ped_fold = ped_split.columns[ped_split.isin([volume_index]).any()][0]
 
+        # If the volume is located in the test split, the volume's masks 
+        # are not inferred
         if ((irf_fold == "1") or (srf_fold == "1") or (ped_fold == "1")):
             print(f"The volume {volume_index} is reserved to fold 1.")
+            return
 
+        # Gets the name of the run in which the validation fold contains the volume to infer 
+        # from the dictionary 
         irf_run = [key for key, val in irf_run_to_val_split.items() if val == int(irf_fold)][1]
         srf_run = [key for key, val in srf_run_to_val_split.items() if val == int(srf_fold)][1]
         ped_run = [key for key, val in ped_run_to_val_split.items() if val == int(ped_fold)][1]
 
+    # In case the split used is the same as the multi-class
     else:
-        folder_to_save = IMAGES_PATH + "\\binary_segmentation_multiclass_split\\"
+        # Declares the name of the folder in which the images will be saved
+        folder_to_save = IMAGES_PATH + "\\OCT_images\\prediction\\binary_segmentation_multiclass_split\\"
+        
+        # Reads the split from used in multi-class segmentation
         split = read_csv("splits\\competitive_fold_selection.csv")
 
+        # Gets the fold in which the volume desired to segment is located
         fold = split.columns[split.isin([volume_index]).any()][0]
         
+        # If the volume is located in the test split, the volume's masks 
+        # are not inferred
         if (fold == "1"):
-            raise ValueError("The selected volume is reserved to fold 1.")
+            print(f"The volume {volume_index} is reserved to fold 1.")
+            return
 
+        # Gets the name of the run in which the validation fold contains the volume to infer 
+        # from the dictionary 
         irf_run = [key for key, val in irf_run_to_val_split.items() if val == int(fold)][0]
         srf_run = [key for key, val in srf_run_to_val_split.items() if val == int(fold)][0]
         ped_run = [key for key, val in ped_run_to_val_split.items() if val == int(fold)][0]
 
+    # The name of the folder is given 
+    # according to the merging strat used
+    if merging_strat == "priority":
+        folder_to_save = folder_to_save + f"Volume{volume_index}_{irf_run}_{srf_run}_{ped_run}_{merging_strat}_{''.join(str(k) for k in order)}\\"
+    elif merging_strat == "softmax":
+        folder_to_save = folder_to_save + f"Volume{volume_index}_{irf_run}_{srf_run}_{ped_run}_{merging_strat}\\"
+    # Creates the folder in case it does not exist yet
+    makedirs(folder_to_save, exist_ok=True)
+
+    # Declares the path to the models that will be used to predict each fluid's mask
     irf_weights_path = f"models\\Run{str(irf_run).zfill(3)}_IRF_UNet3_best_model.pth"
     srf_weights_path = f"models\\Run{str(srf_run).zfill(3)}_SRF_UNet3_best_model.pth"
     ped_weights_path = f"models\\Run{str(ped_run).zfill(3)}_PED_UNet3_best_model.pth"
 
+    # Loads the models to the GPU
     irf_model = UNet(in_channels=1, num_classes=2).to(device=torch.device("cuda"))
     srf_model = UNet(in_channels=1, num_classes=2).to(device=torch.device("cuda"))
     ped_model = UNet(in_channels=1, num_classes=2).to(device=torch.device("cuda"))
 
+    # Loads the weights to the model and assignes the model to the GPU
     irf_model.load_state_dict(torch.load(irf_weights_path, weights_only=True, map_location=torch.device("cuda")))
     irf_model.eval()
     srf_model.load_state_dict(torch.load(srf_weights_path, weights_only=True, map_location=torch.device("cuda")))
@@ -134,17 +169,31 @@ def visualize_binary_segmentations(volume_index: int, binary_split: bool,
     ped_model.load_state_dict(torch.load(ped_weights_path, weights_only=True, map_location=torch.device("cuda")))
     ped_model.eval()
 
+    # Creates the Dataset object as a multi-class since multiple models will have the classes compared
     test_dataset = TestDataset([volume_index], "UNet", "vertical", True, (496,512), None, 1)
+    # Creates the DataLoader object
     test_dataloader = DataLoader(test_dataset, batch_size=1, num_workers=12, collate_fn=collate_fn)
 
+    # Initiates an array that 
+    # will contain the 
+    # Dice-Sorensen coefficient 
+    # for each class in the 
+    # binary segmentations
     slices_dsc_irf = []
     slices_dsc_srf = []
     slices_dsc_ped = []
 
+    # Initiates an array that 
+    # will contain the 
+    # Dice-Sorensen coefficient 
+    # for each class in the 
+    # merged segmentations
     merged_dsc_irf = []
     merged_dsc_srf = []
     merged_dsc_ped = []
 
+    # Indicates that no gradient 
+    # must be calculated in the models
     with torch.no_grad():
         # Creates a progress bar to track the progress on testing images
         with tqdm(test_dataloader, total=len(test_dataloader), desc='Testing Models', unit='img', leave=True, position=0) as progress_bar:
@@ -169,44 +218,66 @@ def visualize_binary_segmentations(volume_index: int, binary_split: bool,
                 srf_outputs = srf_model(images)
                 ped_outputs = ped_model(images)
 
+                # Stacks all the logits
                 logit_stack = torch.stack([irf_outputs, srf_outputs, ped_outputs], axis=-1)
 
+                # Calculates the prediction of each independent model
                 irf_preds = torch.argmax(irf_outputs, dim=1) * 1
                 srf_preds = torch.argmax(srf_outputs, dim=1) * 2
                 ped_preds = torch.argmax(ped_outputs, dim=1) * 3
 
-                overlap_mask = ((irf_outputs > 0).astype(int) +
-                (srf_outputs > 0).astype(int) +
-                (ped_outputs > 0).astype(int)) > 1
+                # Stacks all the preds
+                preds_stack = torch.stack([irf_preds, srf_preds, ped_preds], axis=-1)
 
+                # Creates a binary mask signalling where there 
+                # is overlaps and where there is not
+                overlap_mask = ((irf_preds > 0).astype(int) +
+                (srf_preds > 0).astype(int) +
+                (ped_preds > 0).astype(int)) > 1
+
+                # Initializes the mask of merged predictions as tensor of zeros
+                merged_preds = torch.zeros_like(irf_preds, dtype=uint8).to(device=torch.device("cuda"))
+
+                # In case the merging strat 
+                # selected is "priority"
                 if merging_strat == "priority":
-                    # Initialize the combined mask and overlap mask
-                    merged_preds = zeros_like(irf_preds, dtype=uint8)
-
-                    # Build the combined mask using priority
-                    for class_id in order[::-1]:  # Reverse order: apply lowest priority first
-                        mask = (mask_dict[class_id] > 0).astype(uint8)
+                    # Builds a prediction mask 
+                    # using the input priority
+                    # It iterates in the reverse
+                    # order to apply lowest priority first
+                    for class_id in order[::-1]:
+                        mask = (mask_dict[class_id] > 0).int()
                         merged_preds = where(mask > 0, class_id, merged_preds)
                 
-                    predicted_mask_name = folder_to_save + image_name[:-5] + f"_{irf_run}_{srf_run}_{ped_run}_{merging_strat}_{''.join(str(k) for k in order)}" + ".tiff"
+                # In case the merging strategy 
+                # selected is "softmax"                
                 elif merging_strat == "softmax":
-                    merged_preds = zeros_like(irf_preds, dtype=uint8)
-
+                    # Creates a mask of the pixels 
+                    # where there is no overlapping                
                     non_overlap = ~overlap_mask
-                    merged_preds[non_overlap] = torch.argmax(logit_stack[non_overlap], dim=-1) + 1
-
+                    # Assigns the labels as of the only class where there is no zero
+                    merged_preds[non_overlap] = torch.argmax(preds_stack[non_overlap], dim=-1) + 1
+                    # Gets the indices of where there is overlapping
                     overlap_indices = torch.where(overlap_mask)
+                    # Gets the logits of where there is overlapping
                     overlap_logits = logit_stack[overlap_indices]
+                    # Applies softmax across the different predictions
                     overlap_softmax = torch.softmax(overlap_logits, dim=-1)
+                    # Whichever class has the highest probability (or logit)
+                    # is considered the predicted class
                     fused_classes = torch.argmax(overlap_softmax, dim=-1) + 1
-
+                    # Sets the mask in the overlap pixels to the 
+                    # maximum probability between the predicted classes
                     merged_preds[overlap_indices] = fused_classes
 
-                    predicted_mask_name = folder_to_save + image_name[:-5] + f"_{irf_run}_{srf_run}_{ped_run}_{merging_strat}" + ".tiff"
+                # Declares the name of the predicted mask
+                predicted_mask_name = folder_to_save + image_name[:-5] + ".tiff"
     
+                # Gets the Dice coefficient for each mask
                 dice_scores_irf, _, _, _, _ = dice_coefficient(target=true_masks, prediction=irf_preds, model_name="UNet3", num_classes=2)
                 dice_scores_srf, _, _, _, _ = dice_coefficient(target=true_masks, prediction=srf_preds, model_name="UNet3", num_classes=2)
                 dice_scores_ped, _, _, _, _ = dice_coefficient(target=true_masks, prediction=ped_preds, model_name="UNet3", num_classes=2)
+                # Gets the Dice coefficient for the merged mask
                 dice_scores, _, _, _, _ = dice_coefficient(target=true_masks, prediction=merged_preds, model_name="UNet", num_classes=4)
 
                 # Gets the original OCT B-scan
@@ -238,15 +309,16 @@ def visualize_binary_segmentations(volume_index: int, binary_split: bool,
                 # bounds declared.
                 fluid_norm = mcolors.BoundaryNorm(fluid_bounds, fluid_cmap.N)
 
+                # Initiates a subplot that will have three images along one row
                 fig, axes = plt.subplots(1, 3, figsize=(2 * oct_image.shape[1] / 100, oct_image.shape[0] / 100))
 
-                # Plot the ground-truth masks
+                # Plots the ground-truth masks
                 axes[0].imshow(oct_image, cmap=plt.cm.gray)
                 axes[0].imshow(true_masks, alpha=0.3, cmap=fluid_cmap, norm=fluid_norm)
                 axes[0].axis("off")
                 axes[0].set_title("Ground Truth Masks")
 
-                # Plot the predicted masks
+                # Plots the predicted masks and the overlaying in purple
                 axes[1].imshow(oct_image, cmap=plt.cm.gray)
                 axes[1].imshow(irf_preds, alpha=0.3, cmap=fluid_cmap, norm=fluid_norm)
                 axes[1].imshow(srf_preds, alpha=0.3, cmap=fluid_cmap, norm=fluid_norm)
@@ -255,21 +327,25 @@ def visualize_binary_segmentations(volume_index: int, binary_split: bool,
                 axes[1].axis("off")
                 axes[1].set_title(f"Predicted Masks. DSC: {dice_scores_irf[1]}, {dice_scores_srf[1]}, {dice_scores_ped[1]}")
 
-                # Plot the predicted masks
+                # Plots the predicted masks merged
                 axes[2].imshow(oct_image, cmap=plt.cm.gray)
                 axes[2].imshow(merged_preds, alpha=0.3, cmap=fluid_cmap, norm=fluid_norm)
                 axes[2].axis("off")
                 axes[2].set_title(f"Merged Masks. DSC: {dice_scores[1:3]}")
 
-                # Save the combined figure
+                # Saves the combined figures
                 plt.savefig(predicted_mask_name, bbox_inches='tight', pad_inches=0)
                 plt.clf()
                 plt.close("all")
 
+                # Appends the Dice coefficient for the 
+                # slice in the binary models
                 slices_dsc_irf.append(dice_scores_irf[1])
                 slices_dsc_srf.append(dice_scores_srf[1])
                 slices_dsc_ped.append(dice_scores_ped[1])
 
+                # Appends the Dice coefficient for the 
+                # slice with the merged binary models
                 merged_dsc_irf.append(dice_scores[1])
                 merged_dsc_srf.append(dice_scores[2])
                 merged_dsc_ped.append(dice_scores[3])
@@ -277,6 +353,8 @@ def visualize_binary_segmentations(volume_index: int, binary_split: bool,
                 # Update the progress bar
                 progress_bar.update(1)
 
+    # Prints the statistics of the model
+    print(f"Volume {volume_index}")
 
     print(f"IRF: {array(slices_dsc_irf).mean()}")
     print(f"SRF: {array(slices_dsc_srf).mean()}")
