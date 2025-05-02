changed the data normalization to 0-1 range, the weights initialization in the networks, corrected the GAN evaluation, adapted the test file to also be used to pix2pix #100 #101

network_functions/dataset.py
@@ -1256,7 +1256,7 @@ class TrainDatasetGAN(Dataset):
         image_path = images_folder + self.images_names[index]
 
         # Reads the middle image as a NumPy array
-        img = torch.from_numpy(imread(image_path))
+        img = torch.from_numpy(imread(image_path) / 255.).float()
         # Gets the number of the slice
         img_number = int(image_path.split(".")[0][-3:])
         # Sets the name of the previous and following images
@@ -1265,8 +1265,8 @@ class TrainDatasetGAN(Dataset):
 
         # Loads the previous and the 
         # following images 
-        prev_img = torch.from_numpy(imread(prev_img_path))
-        next_img = torch.from_numpy(imread(next_img_path))
+        prev_img = torch.from_numpy(imread(prev_img_path) / 255.).float()
+        next_img = torch.from_numpy(imread(next_img_path) / 255.).float()
 
         # Organizes the data in a dictionary that contains the images 
         # stacked along the first axis under the key "stack"
@@ -1342,7 +1342,7 @@ class ValidationDatasetGAN(Dataset):
         image_path = images_folder + self.images_names[index]
 
         # Reads the middle image as a NumPy array
-        img = torch.from_numpy(imread(image_path))
+        img = torch.from_numpy(imread(image_path) / 255).float()
         # Gets the number of the slice
         img_number = int(image_path.split(".")[0][-3:])
         # Sets the name of the previous and following images
@@ -1351,8 +1351,8 @@ class ValidationDatasetGAN(Dataset):
 
         # Loads the previous and the 
         # following images 
-        prev_img = torch.from_numpy(imread(prev_img_path))
-        next_img = torch.from_numpy(imread(next_img_path))
+        prev_img = torch.from_numpy(imread(prev_img_path) / 255).float()
+        next_img = torch.from_numpy(imread(next_img_path) / 255).float()
 
         # Organizes the data in a dictionary that contains the images 
         # stacked along the first axis under the key "stack"
@@ -1428,7 +1428,7 @@ class TestDatasetGAN(Dataset):
         image_path = images_folder + self.images_names[index]
 
         # Reads the middle image as a NumPy array
-        img = torch.from_numpy(imread(image_path))
+        img = torch.from_numpy(imread(image_path) / 255.).float()
         # Gets the number of the slice
         img_number = int(image_path.split(".")[0][-3:])
         # Sets the name of the previous and following images
@@ -1437,8 +1437,8 @@ class TestDatasetGAN(Dataset):
 
         # Loads the previous and the 
         # following images 
-        prev_img = torch.from_numpy(imread(prev_img_path))
-        next_img = torch.from_numpy(imread(next_img_path))
+        prev_img = torch.from_numpy(imread(prev_img_path) / 255.).float()
+        next_img = torch.from_numpy(imread(next_img_path) / 255.).float()
 
         # Organizes the data in a dictionary that contains the images stacked along the first axis under the key 
         # "stack" and the path of the middle image associated with the key "image_name"

test_gan.py
@@ -11,6 +11,8 @@ from torch.utils.data import DataLoader
 from networks.gan import Generator
 from networks.loss import psnr
 from network_functions.dataset import TestDatasetGAN
+from networks.pix2pix import Pix2PixGenerator
+from networks.unet import UNet
 from paths import IMAGES_PATH
 
 # Imports tqdm depending on whether 
@@ -163,41 +165,59 @@ def folds_results_gan(first_run_name: str, iteration: int, k: int=5):
     results_df.to_csv(f".\\results\\Iteration{iteration}_results_gan.csv")
 
 def test_gan(
-        fold_test: int, 
+        fold_test: int,
+        model_name: str, 
         weights_name: str, 
         batch_size: int=1, 
         device_name: str="GPU", 
         final_test: bool=False, 
+        gen_model_name: str=None,
         number_of_classes: int=1,
-        save_images: bool=True
+        save_images: bool=True,
+        trained_generator_name: str=None
     ):
     """
-    Function used to test the trained GAN models
+    Function used to test all the trained generative models
 
     Args:
         fold_test (int): number of the fold that will be used 
             in the network testing 
+        model_name (str): name of the model desired to test.
+            Can only be 'GAN', 'UNet', or 'Pix2Pix'
         weights_name (str): path to the model's weight file
         batch_size (int): size of the batch used in testing
         device_name (str): indicates whether the network will 
             be trained using the CPU or the GPU
-        final_test (bool): indicates that the final test will be 
-            performed, changing the name of the saved files. Since final
-            testing is rare, the default value is False 
+        final_test (bool): indicates that the final test will 
+            be performed, changing the name of the saved files. 
+            Since final testing is rare, the default value is 
+            False
+        gen_model_name (str): name of the trained generator 
+            model used in the Pix2Pix. Can only be 'GAN' or
+            'UNet'
         number_of_classes (int): number of classes the 
             output will present
         save_images (bool): flag that indicates whether the 
             predicted images will be saved or not
+        trained_generator_name (str): name of the trained 
+            generator weights file
             
     Return:
         None
     """
     # Gets the list of volumes used to test the model
-    df = read_csv("splits/generation_5_fold_selection.csv")
+    df = read_csv("splits/generation_4_fold_selection.csv")
     test_volumes = df[str(fold_test)].dropna().to_list()
 
+    # Checks if the given model name is available
+    assert model_name in ["GAN", "UNet", "Pix2Pix"],\
+        "Possible model names: 'GAN', 'UNet', or 'Pix2Pix'"
+
     # Declares the Generator model that will be used to infer
-    generator = Generator(number_of_classes)
+    if model_name == "GAN":
+        generator = Generator(number_of_classes)
+    elif model_name == "UNet":
+        generator = UNet(in_channels=2, num_classes=number_of_classes)
 
     # Declares the path to the weights
     weights_path = "models\\" + weights_name
@@ -221,7 +241,21 @@ def test_gan(
     # Assigns the generator to the selected device
     generator = generator.to(device=device)
     # Loads the model's trained weights
-    generator.load_state_dict(torch.load(weights_name, weights_only=True, map_location=device))
+    if trained_generator_name == None:
+        generator.load_state_dict(torch.load(weights_path, weights_only=True, map_location=device))
+    else:
+        # Declares the path to the weights of the trained generator
+        gen_weights_path = "models\\" + trained_generator_name
+        # Loads the weights to the trained generator
+        generator.load_state_dict(torch.load(gen_weights_path, weights_only=True, map_location=device))
+        # Initiates the Pix2Pix generator
+        pix2pix_generator = Pix2PixGenerator(number_of_classes, number_of_classes)
+        # Loads the weights to the trained Pix2Pix generator
+        pix2pix_generator.load_state_dict(torch.load(weights_path, weights_only=True, map_location=device))
+        # Sets the Pix2Pix generator 
+        # to evaluation mode
+        pix2pix_generator.eval()
+
     # Sets the generator 
     # to evaluation mode
     generator.eval()
@@ -274,15 +308,30 @@ def test_gan(
                 mid_imgs = stack[:,1,:,:].to(device=device)
                 next_imgs = stack[:,2,:,:].to(device=device)
 
+                # Using the trained generator, the previous and 
+                # following images are used to generate the middle image
+                if gen_model_name == "GAN":
+                    gen_imgs_wgenerator = generator(prev_imgs.detach(), next_imgs.detach())
+                elif gen_model_name == "UNet":
+                    gen_imgs_wgenerator = generator(torch.stack([prev_imgs, next_imgs], dim=1).detach())
+                elif not None:
+                    print("Unrecognized generator model name. Available names: 'GAN' or 'UNet'")
+                    return
+
                 # Generates the middle image
-                gen_imgs = generator(prev_imgs, next_imgs)
+                if model_name == "GAN":
+                    gen_imgs = generator(prev_imgs.detach(), next_imgs.detach())
+                elif model_name == "UNet":
+                    gen_imgs = generator(torch.stack([prev_imgs, next_imgs], dim=1).detach().to(device=device))
+                elif model_name == "Pix2Pix":
+                    gen_imgs = pix2pix_generator(gen_imgs_wgenerator)
 
                 # Calculates the PSNR value
                 img_psnr = round(psnr(mid_imgs, gen_imgs), 3)
                 # Calculates the SSIM value
                 img_ssim = round(ssim(
                                 mid_imgs, gen_imgs,
-                                data_range=255,
+                                data_range=1,
                                 gaussian_weights=True,
                                 use_sample_covariance=False,
                                 win_size=11

train_pix2pix.py
@@ -11,6 +11,7 @@ from networks.gan import Generator
 from networks.loss import discriminator_loss, generator_loss
 from networks.pix2pix import Pix2PixDiscriminator, Pix2PixGenerator
 from networks.unet import UNet
+from train_gan import weights_normal_initialization
 
 # Imports tqdm depending on whether 
 # it is being called from the 
@@ -39,7 +40,7 @@ def train_pix2pix(
         split: str="generation_5_fold_split.csv",
 ):
     """
-    Function that trains the GAN models.
+    Function that trains the Pix2Pix GAN models.
 
     Args:
         run_name (str): name of the run under which the best model
@@ -143,6 +144,11 @@ def train_pix2pix(
     pix2pix_generator = Pix2PixGenerator(number_of_classes, number_of_classes)
     pix2pix_discriminator = Pix2PixDiscriminator(number_of_classes)
 
+    # Initiates the weights of the Pix2Pix generator 
+    # and discriminator
+    pix2pix_generator.apply(weights_normal_initialization)
+    pix2pix_discriminator.apply(weights_normal_initialization)
+
     # Declares the optimizer of the Generator
     optimizer_G = torch.optim.Adam(pix2pix_generator.parameters(), 
                                 lr=learning_rate, 
@@ -227,7 +233,7 @@ def train_pix2pix(
                 if model_name == "GAN":
                     gen_imgs_wgenerator = generator(prev_imgs.detach(), next_imgs.detach())
                 elif model_name == "UNet":
-                    gen_imgs_wgenerator = generator(torch.stack([prev_imgs, next_imgs], dim=1).detach().float() / 255.0)
+                    gen_imgs_wgenerator = generator(torch.stack([prev_imgs, next_imgs], dim=1).detach())
                 # Sets the label associated with true images to 0.95. The reason 
                 # this value is set to 0.95 and not 1 is called label smoothing and 
                 # prevents the model of becoming too overconfident, improving training 
@@ -260,13 +266,13 @@ def train_pix2pix(
 
                 # Gets the prediction of the discriminator 
                 # on whether the true image is true or fake                
-                gt_distingue = pix2pix_discriminator(torch.cat((gen_imgs_wgenerator.detach(), gen_imgs), dim=1))
+                gt_distingue = pix2pix_discriminator(torch.cat((gen_imgs_wgenerator.detach(), mid_imgs), dim=1))
                 # Gets the prediction of the discriminator 
                 # on whether the fake image is true or fake
                 # The generated image is detached so that 
                 # the gradient of the discriminator does not 
                 # affect the generator function
-                fake_distingue = pix2pix_discriminator(torch.cat((gen_imgs_wgenerator.detach(), mid_imgs.detach())))
+                fake_distingue = pix2pix_discriminator(torch.cat((gen_imgs_wgenerator.detach(), gen_imgs.detach()), dim=1))
                 # The discriminator loss is calculated for the true image and fake image predicted labels 
                 # and their respective true labels
                 real_loss, fake_loss, d_loss = discriminator_loss(device, gt_distingue, fake_distingue, valid, fake)
@@ -300,28 +306,25 @@ def train_pix2pix(
 
                 
         print(f"Validating Epoch {epoch}")
-        # Calls the function that evaluates the output of the generator and returns the 
-        # respective result of the peak signal-to-noise ratio (PSNR)
-        val_psnr = evaluate_gan(model_name=model_name, generator=pix2pix_generator, dataloader=val_loader, device=device)
+        # Calls the function that evaluates the output of the Pix2Pix generator 
+        # and returns the respective result of the peak signal-to-noise ratio (PSNR)
+        val_psnr = evaluate_gan(model_name=model_name, dataloader=val_loader, 
+                                device=device, pix2pix_generator=pix2pix_generator, 
+                                generator=generator)
 
         # Logs the results of the validation
         logging.info(f"Validation Mean PSNR: {val_psnr}")
 
         # Writes the mean of the results of the different batches in 
         # the epoch to the CSV file 
-        if model_name == "GAN":
-            with open(csv_epoch_filename, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerow([epoch, epoch_adv_loss.item() / len(val_loader), 
-                                epoch_g_loss.item() / len(val_loader), 
-                                epoch_real_loss.item() / len(val_loader), 
-                                epoch_fake_loss.item() / len(val_loader), 
-                                epoch_d_loss.item() / len(val_loader),
-                                val_psnr])
-        elif model_name == "UNet":
-            with open(csv_epoch_filename, mode="a", newline="") as file:
-                writer = csv.writer(file)
-                writer.writerow([epoch, val_psnr])
+        with open(csv_epoch_filename, mode="a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([epoch, epoch_adv_loss.item() / len(val_loader), 
+                            epoch_g_loss.item() / len(val_loader), 
+                            epoch_real_loss.item() / len(val_loader), 
+                            epoch_fake_loss.item() / len(val_loader), 
+                            epoch_d_loss.item() / len(val_loader),
+                            val_psnr])
 
         # In case the validation SSIM is better 
         # than the previous, the model is saved 